fe36f80916a087cdc9cc42840ac0341d272c1c6c
Basil Ugo <basilugo2@gmail.com>
2025-01-30 13:00:14 +0100

    update tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,66 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
+	darkMode: ["class"],
+	content: [
+		"app/**/*.{ts,tsx}",
+		"components/**/*.{ts,tsx}",
+		"./**/*.html",
+		"./js/**/*.js",
+	],
+	theme: {
+		extend: {
+			colors: {
+				border: "hsl(var(--border))",
+				input: "hsl(var(--input))",
+				ring: "hsl(var(--ring))",
+				background: "hsl(var(--background))",
+				foreground: "hsl(var(--foreground))",
+				primary: {
+					DEFAULT: "hsl(var(--primary))",
+					foreground: "hsl(var(--primary-foreground))",
+				},
+				secondary: {
+					DEFAULT: "hsl(var(--secondary))",
+					foreground: "hsl(var(--secondary-foreground))",
+				},
+				destructive: {
+					DEFAULT: "hsl(var(--destructive))",
+					foreground: "hsl(var(--destructive-foreground))",
+				},
+				muted: {
+					DEFAULT: "hsl(var(--muted))",
+					foreground: "hsl(var(--muted-foreground))",
+				},
+				accent: {
+					DEFAULT: "hsl(var(--accent))",
+					foreground: "hsl(var(--accent-foreground))",
+				},
+				popover: {
+					DEFAULT: "hsl(var(--popover))",
+					foreground: "hsl(var(--popover-foreground))",
+				},
+				card: {
+					DEFAULT: "hsl(var(--card))",
+					foreground: "hsl(var(--card-foreground))",
+				},
+				text: {
+					DEFAULT: "var(--color-text)",
+					offset: "var(--color-text-offset)",
+				},
+				background: {
+					DEFAULT: "var(--color-background)",
+					offset: "var(--color-background-offset)",
+				},
+			},
+			borderRadius: {
+				lg: "var(--radius)",
+				md: "calc(var(--radius) - 2px)",
+				sm: "calc(var(--radius) - 4px)",
+			},
+		},
+	},
+	variants: {
+		extend: {},
+	},
+	plugins: [require("tailwindcss-animate")],
+};
